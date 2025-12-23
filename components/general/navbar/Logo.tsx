76403c19b0b5d2ab54b13be65173060954708014
Serg <srdan.uzelac.smartbit@gmail.com>
2025-12-23 17:46:19 +0100

    003

--- components/general/navbar/Logo.tsx
+++ components/general/navbar/Logo.tsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 import { LuCodeXml } from "react-icons/lu";
 
 export default function Logo() {
-	return (
-		<Link href="/" className="flex items-center space-x-2">
-			<div className="w-10 h-10 bg-gray-200 rounded-full grid place-items-center">
-				<LuCodeXml />
-			</div>
-			<p className="text-gray-200 font-bold md:text-2xl sm:block hidden">Webdev</p>
-		</Link >
-	)
+  return (
+    <Link href="/" className="flex items-center space-x-2">
+      <div className="w-10 h-10 bg-gray-200 rounded-full grid place-items-center">
+        <LuCodeXml />
+      </div>
+      <p className="text-gray-200 font-bold md:text-2xl xl:block hidden">
+        Webdev
+      </p>
+    </Link>
+  );
 }
--- components/general/navbar/Navbar.tsx
+++ components/general/navbar/Navbar.tsx
@@ -1,13 +1,54 @@
-import Logo from "./Logo"
+import Link from "next/link";
+import Logo from "./Logo";
+import LinkButton from "../LinkButton";
+import { LuDownload } from "react-icons/lu";
+
+const navLinks = [
+  { url: "#home", label: "Home" },
+  { url: "#services", label: "Services" },
+  { url: "#resume", label: "Resume" },
+  { url: "#projects", label: "Projects" },
+  { url: "#skilla", label: "Skills" },
+  { url: "#testimonials", label: "Testimonials" },
+  { url: "#contact", label: "Contact" },
+];
 
 export default function Navbar() {
-	return (
-		<nav className="h-18 fixed z-50 w-full">
-			<div className="flex items-center h-full justify-between w-[90%] m-auto">
-				{/* LOGO */}
-				<Logo />
-			</div>
-
-		</nav>
-	)
+  return (
+    <nav className="h-18 fixed z-50 w-full">
+      <div
+        className="flex items-center h-full justify-between 
+			w-[90%] m-auto"
+      >
+        {/* LOGO */}
+        <Logo />
+        {/* LINKS */}
+        <ul className=" lg:flex space-x-10 hidden">
+          {navLinks.map((link) => {
+            return (
+              <li key={link.url}>
+                <Link
+                  className="text-gray-200 hover:text-cyan-300
+									font-medium transition duration-500"
+                  href={link.url}
+                >
+                  {link.label}{" "}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+        {/* BUTTON */}
+        <div className="hidden lg:block">
+          <LinkButton
+            href="/documents/cv.pdf"
+            text="Download CV"
+            download
+            icon={LuDownload}
+            iconPosition="left"
+          />
+        </div>
+      </div>
+    </nav>
+  );
 }
